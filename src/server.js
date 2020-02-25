c27f81b95ac4a8b2e6009b6bd0a85855af44eb15
odunayo <okebunmiodunayo@gmail.com>
2020-02-25 15:56:17 +0100

    setup tests

--- src/server.js
+++ src/server.js
@@ -1,16 +1,21 @@
 import express from 'express';
 import Debug from 'debug';
 import logger from 'morgan';
+import bodyParser from 'body-parser';
+
 
 const debug = Debug('dev');
 const app = express();
 
 app.use(logger('dev'));
 app.use(express.json());
-app.get('/', (req, res) => res.status(200).send({ message: 'Hello World' }));
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: false }));
+
+app.get('/', (req, res) => res.status(200).send({ message: 'Welcome to NAREP API' }));
 
 const server = app.listen(process.env.PORT || 3000, () => {
   debug(`Listening on port ${server.address().port}`);
 });
 
-// export default app;
+export default app;
