dfa3f814f6a151e09f90590377b5b934feebebb1
odunayo <okebunmiodunayo@gmail.com>
2020-03-12 10:40:48 +0100

    user should be able to get all files, also get files by sector, by tag

--- src/controller/fileController.js
+++ src/controller/fileController.js
@@ -1,21 +1,27 @@
-/* eslint-disable import/prefer-default-export */
+import { Op } from 'sequelize';
 import fs from 'fs';
 import model from '../database/models';
 import { errorResponse, successResponse } from '../helpers/serverResponse';
-import { createUniqueSlug, createFileExtension, generateTag } from '../helpers/utils';
+import {
+  createUniqueSlug, createFileExtension, generateTag, pagination,
+} from '../helpers/utils';
 
 global.appRoot = __dirname;
 const {
-  File, Sector,
+  File, Sector, User, Tag,
 } = model;
 
 export const uploadFile = async (req, res, next) => {
   try {
     const {
-      title, description, sector, tags,
-    } = req.body;
-    const { filename, path, mimetype } = req.file;
-    const { id } = req.user;
+      body:
+      {
+        title, description, sector, tags,
+      },
+
+      file: { filename, path, mimetype },
+      user: { id },
+    } = req;
     const findFile = await File.findOne({ where: { title } });
 
     if (findFile) {
@@ -45,6 +51,123 @@ export const uploadFile = async (req, res, next) => {
 
     fs.rename(path, file, () => (createdFile));
     return successResponse(res, 201, 'file', { message: 'file has been created successfully!', createdFile });
+  } catch (error) {
+    return next(error);
+  }
+};
+
+export const getFiles = async (req, res, next) => {
+  try {
+    const { query: { page, limit } } = req;
+    const pageNumber = pagination(page, limit);
+    const files = await File.findAndCountAll({
+      offset: pageNumber.offset,
+      limit: pageNumber.limit,
+      order: [['updatedAt', 'DESC']],
+      subQuery: false,
+      include: [
+        {
+          model: User,
+          as: 'user',
+          attributes: ['id', 'username'],
+        },
+      ],
+    });
+    const { rows: allFiles, count: filesCount } = files;
+    return successResponse(res, 200, 'files', { filesCount, allFiles });
+  } catch (error) {
+    return next(error);
+  }
+};
+
+export const getFilesBySector = async (req, res, next) => {
+  const { params: { sectorId }, query: { page, limit } } = req;
+  try {
+    const pageNumber = pagination(page, limit);
+    const files = await File.findAndCountAll({
+      offset: pageNumber.offset,
+      limit: pageNumber.limit,
+      order: [['updatedAt', 'DESC']],
+      include: [
+        {
+          model: User,
+          as: 'user',
+          attributes: ['id', 'username'],
+        },
+      ],
+      where: { sectorId },
+    });
+    const { rows: allFiles, count: filesCount } = files;
+    return successResponse(res, 200, 'files', { filesCount, allFiles });
+  } catch (error) {
+    return next(error);
+  }
+};
+
+export const getFilesByTag = async (req, res, next) => {
+  const { params: { id } } = req;
+  try {
+    const files = await Tag.findAndCountAll({
+      order: [['updatedAt', 'DESC']],
+      attributes: ['id', 'name'],
+      include: [
+        {
+          model: File,
+          as: 'files',
+          through: { attributes: [] },
+        },
+      ],
+      where: { id },
+    });
+    const { rows: allFiles, count: filesCount } = files;
+    return successResponse(res, 200, 'files', { filesCount, allFiles });
+  } catch (error) {
+    return next(error);
+  }
+};
+
+export const searchFile = async (req, res, next) => {
+  const {
+    query: { title, tag, author },
+  } = req;
+  const keyword = title || '';
+  const tagValue = tag || '';
+  const authorValue = author || '';
+
+  try {
+    const files = await File.findAndCountAll({
+      where: {
+        title: {
+          [Op.iLike]: `%${keyword}%`,
+        },
+      },
+      attributes: ['id', 'title', 'description', 'slug'],
+      include: [
+        {
+          model: User,
+          as: 'user',
+          where: {
+            username: {
+              [Op.iLike]: `%${authorValue}%`,
+            },
+          },
+          attributes: ['id', 'username'],
+        },
+        {
+          model: Tag,
+          as: 'tags',
+          through: { attributes: [] },
+          where: {
+            name: {
+              [Op.iLike]: `%${tagValue}%`,
+            },
+          },
+        },
+      ]
+      ,
+    });
+    const { rows: allFiles, count: filesCount } = files;
+    return successResponse(res, 200, 'files', { filesCount, allFiles });
   } catch (error) {
     return next(error);
   }
---
+++ src/tests/files/getFiles.spec.js
@@ -0,0 +1,53 @@
+import chai from 'chai';
+import chaiHttp from 'chai-http';
+import app from '../../server';
+
+chai.use(chaiHttp);
+
+const { expect } = chai;
+const server = () => chai.request(app);
+const API_PREFIX = '/api/v1/files';
+const sectorId = '30b113d2-b616-4ea7-8b73-a53a6015f321';
+const tagId = '59fe8928-37e4-4bd9-a04e-011e3be32fb5';
+
+describe('Get Files', () => {
+  it('should get all files successfully', (done) => {
+    server()
+      .get(`${API_PREFIX}`)
+      .end((err, res) => {
+        expect(res.status).to.be.eql(200);
+        expect(res.body).to.have.property('files');
+        expect(res.body.files).to.be.to.a('object');
+        expect(res.body.files).to.have.property('filesCount');
+        expect(res.body.files).to.have.property('allFiles');
+        expect(res.body.files.allFiles).to.be.to.an('array');
+        done();
+      });
+  });
+  it('should get all files by sector successfully', (done) => {
+    server()
+      .get(`${API_PREFIX}/sectors/${sectorId}`)
+      .end((err, res) => {
+        expect(res.status).to.be.eql(200);
+        expect(res.body).to.have.property('files');
+        expect(res.body.files).to.be.to.a('object');
+        expect(res.body.files).to.have.property('filesCount');
+        expect(res.body.files).to.have.property('allFiles');
+        expect(res.body.files.allFiles).to.be.to.an('array');
+        done();
+      });
+  });
+  it('should get all files by tag successfully', (done) => {
+    server()
+      .get(`${API_PREFIX}/tags/${tagId}`)
+      .end((err, res) => {
+        expect(res.status).to.be.eql(200);
+        expect(res.body).to.have.property('files');
+        expect(res.body.files).to.be.to.a('object');
+        expect(res.body.files).to.have.property('filesCount');
+        expect(res.body.files).to.have.property('allFiles');
+        expect(res.body.files.allFiles).to.be.to.an('array');
+        done();
+      });
+  });
+});
