6b1a07ca8cd5be1b69689504eb01f20530114494
odunayo <okebunmiodunayo@gmail.com>
2020-03-13 11:46:43 +0100

    added new routes to swagger documentation

--- src/docs/index.js
+++ src/docs/index.js
@@ -3,9 +3,19 @@ import signup from './auth/signup.json';
 import signin from './auth/signin.json';
 
 import uploadFiles from './files/uploadFiles.json';
+import getFiles from './files/getFiles.json';
+import getFilesByTag from './files/getFilesByTagId.json';
+import getFilesBySector from './files/getFilesBySectorId.json';
+
+import downloadFiles from './files/downloadFiles.json';
 
 swagger.paths['/auth/signup'] = signup;
 swagger.paths['/auth/signin'] = signin;
 swagger.paths['/files/uploads'] = uploadFiles;
+swagger.paths['/files'] = getFiles;
+swagger.paths['/files/tags/{id}'] = getFilesByTag;
+swagger.paths['/files/sectors/{sectorId}'] = getFilesBySector;
+swagger.paths['/files/downloads'] = downloadFiles;
+
 
 export default swagger;
