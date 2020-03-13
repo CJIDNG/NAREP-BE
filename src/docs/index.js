7c20879c6195e9dc5c30eac5b7da0077c31757b6
odunayo <okebunmiodunayo@gmail.com>
2020-03-13 17:33:35 +0100

    add swagger docs

--- src/docs/index.js
+++ src/docs/index.js
@@ -6,6 +6,7 @@ import uploadFiles from './files/uploadFiles.json';
 import getFiles from './files/getFiles.json';
 import getFilesByTag from './files/getFilesByTagId.json';
 import getFilesBySector from './files/getFilesBySectorId.json';
+import getFilesBySlug from './files/getFIlesBySlug.json';
 
 import downloadFiles from './files/downloadFiles.json';
 
@@ -15,12 +16,18 @@ import deleteFileBySlug from './files/deleteFIles.json';
 
 swagger.paths['/auth/signup'] = signup;
 swagger.paths['/auth/signin'] = signin;
+
 swagger.paths['/files/uploads'] = uploadFiles;
+
 swagger.paths['/files'] = getFiles;
 swagger.paths['/files/tags/{id}'] = getFilesByTag;
+swagger.paths['/files/{slug}'] = getFilesBySlug;
 swagger.paths['/files/sectors/{sectorId}'] = getFilesBySector;
+
 swagger.paths['/files/downloads'] = downloadFiles;
+
 swagger.paths['/files/{slug}'] = updateFileBySlug;
+
 swagger.paths['/files/{slug}'] = deleteFileBySlug;
 
 export default swagger;
