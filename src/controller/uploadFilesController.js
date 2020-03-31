d927e1dca4b8a8d458c5caa08c7cfe1e1614d2d8
odunayo <okebunmiodunayo@gmail.com>
2020-03-31 21:07:27 +0100

    bug fix

--- src/controller/uploadFilesController.js
+++ src/controller/uploadFilesController.js
@@ -21,7 +21,6 @@ export const uploadFile = async (req, res, next) => {
       },
       user: { id },
     } = req;
-    console.log(req.file);
     const findFile = await File.findOne({ where: { title } });
 
     if (findFile) {
